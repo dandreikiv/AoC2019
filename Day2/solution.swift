a817c2a378eb99d8b1d18b768278b3d22418598a
Dmytro Andreikiv <dmytro.andreikiv@ah.nl>
2019-12-02 10:09:22 +0100

    AoC 2019 day2 part2

--- Day2/solution.swift
+++ Day2/solution.swift
@@ -4,26 +4,40 @@ let url = URL(fileURLWithPath: "input.txt")
 let string = try String(contentsOf: url)
 var a = string.split(separator: ",").compactMap{Int($0)}
 
-a[1] = 12
-a[2] = 2
+func computeProgram(_ a: [Int], noun: Int, verb: Int) -> Int {
+    var a = a
 
-var pointer = 0
+    a[1] = noun
+    a[2] = verb
 
-while pointer < a.count - 4{
-    let op = a[pointer]
-    let pi1 = a[pointer + 1]
-    let pi2 = a[pointer + 2]
-    let di = a[pointer + 3]
+    var pointer = 0
 
-    if op == 1 {
-        a[di] = a[pi1] + a[pi2]
-    } else if op == 2 {
-        a[di] = a[pi1] * a[pi2]
-    } else if op == 99 {
-        break
+    while pointer < a.count - 4{
+        let op = a[pointer]
+        let pi1 = a[pointer + 1]
+        let pi2 = a[pointer + 2]
+        let di = a[pointer + 3]
+
+        if op == 1 {
+            a[di] = a[pi1] + a[pi2]
+        } else if op == 2 {
+            a[di] = a[pi1] * a[pi2]
+        } else if op == 99 {
+            break
+        }
+
+        pointer += 4
     }
-
-    pointer += 4
+    
+    return a[0]
 }
 
-print(a[0])
+for noun in 0...99 {
+    for verb in 0...99 {
+        let result =  computeProgram(a, noun: noun, verb: verb)
+        if result == 19690720 {
+            print("result: \(noun * 100 + verb)")
+            break
+        }
+    }
+}
