b36228ecd0cf57028bfc6028507011080f3efc4a
Dmytro Andreikiv <dmytro.andreikiv@ah.nl>
2019-12-04 07:59:22 +0100

    AoC 2019 Day4 part2

--- Day4/solution.swift
+++ Day4/solution.swift
@@ -1,42 +1,37 @@
 import Foundation
 
 /**
-It is a six-digit number.
-The value is within the range given in your puzzle input.
-Two adjacent digits are the same (like 22 in 122345).
-Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-Other than the range rule, the following are true:
+An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
 
-111111 meets these criteria (double 11, never decreases).
-223450 does not meet these criteria (decreasing pair of digits 50).
-123789 does not meet these criteria (no double).
-How many different passwords within the range given in your puzzle input meet these criteria?
+Given this additional criterion, but still ignoring the range rule, the following are now true:
+
+112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
 
 Your puzzle input is 168630-718098.
 */
 var count = 0
 for i in 168630...718098 {
-    let s = i.description
-    let digits = s.compactMap { Int(String($0)) }
+    let digits = i.description.compactMap { Int(String($0)) }
+    let adjacentGroup = NSCountedSet(array: digits)
 
-    var hasAdjacent = false
     var increasing = true
-    var prevDigit = digits[0]
-
-    for d in digits[1...] {
-        if d < prevDigit {
+    for idx in 0..<digits.count - 1 {
+        if digits[idx] > digits[idx + 1] {
             increasing = false
             break
         }
-
-        if d == prevDigit {
-            hasAdjacent = true
-        }
-
-        prevDigit = d
     }
 
-    if hasAdjacent && increasing {
+    var hasGroupOfTwo = false
+    for digit in adjacentGroup {
+        if adjacentGroup.count(for: digit) == 2 {
+            hasGroupOfTwo = true
+            break
+        } 
+    }
+    if increasing && hasGroupOfTwo {
         count+=1
     }
 }
